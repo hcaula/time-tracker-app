c01d99b4320f429fe4b949d49e8ce8e92468f757
Lucas Barros <lbam@cin.ufpe.br>
2018-01-28 16:04:01 -0300

    Install request and request-promise package

--- src/components/GoogleLoginBtn.js
+++ src/components/GoogleLoginBtn.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'semantic-ui-react'
-
+import { config } from '../config'
+ 
 class GoogleLoginBtn extends Component {
+  
+  login(e){
+
+  }
+
   render() {
     return (
-      <Button color='google plus'><Icon name='google'></Icon>Sign in with Google</Button>
+      <Button onClick={login} color='google plus'><Icon name='google'></Icon>Sign in with Google</Button>
     );
   }
 }
