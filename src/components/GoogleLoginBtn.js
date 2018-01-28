9f6aa8fe6a4b4207cf8c3ff1abf812a6dec4088c
Lucas Barros <lbam@cin.ufpe.br>
2018-01-28 16:37:06 -0300

    Add login function to google button

--- src/components/GoogleLoginBtn.js
+++ src/components/GoogleLoginBtn.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'semantic-ui-react'
 import { config } from '../config'
- 
+import rp from 'request-promise'
+
 class GoogleLoginBtn extends Component {
   
   login(e){
+    var request = {
+      uri: config.api_url + '/login',
+      method: 'GET'
+    }
 
+    rp(request).then(() => console.log("Alright!")).catch((err) => console.log(err));
   }
 
   render() {
     return (
-      <Button onClick={login} color='google plus'><Icon name='google'></Icon>Sign in with Google</Button>
+      <Button onClick={this.login} color='google plus'><Icon name='google'></Icon>Sign in with Google</Button>
     );
   }
 }
